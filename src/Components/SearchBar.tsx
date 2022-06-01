e4da8e6bc139cc1cabd9514c906ce2ac9f8ba464
Emilia Starczyk <emiliastarczyk@gmail.com>
2022-06-01 10:02:57 +0200

    conversion for filtering

--- src/Components/SearchBar.tsx
+++ src/Components/SearchBar.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 export type SearchBarProps = {
     data: any[],
     filterFunction: (object: any) => string,
@@ -8,6 +8,16 @@ export type SearchBarProps = {
 export default function SearchBar(props: SearchBarProps) {
     const [convertedData, setConvertedData] = useState<any[]>();
     const [filters, setFilters] = useState<any[]>();
+
+    useEffect(() => {
+        let newData: any[] = new Array(0);
+        props.data.forEach(element => {
+            newData.push({ text: props.filterFunction(element), data: element });
+        });
+        console.log(newData);
+        setConvertedData(newData);
+    }, [])
+
     return (
         <div>SearchBar</div>
     )
