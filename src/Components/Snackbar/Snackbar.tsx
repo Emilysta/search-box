37f077a57ee93f36f2235a520acba58d5d3b26a1
Emilia Starczyk <emiliastarczyk@gmail.com>
2022-06-27 09:52:41 +0200

    Config for css modules

--- src/Components/Snackbar/Snackbar.tsx
+++ src/Components/Snackbar/Snackbar.tsx
@@ -1,9 +1,9 @@
-import './Snackbar.css';
+import styles from './Snackbar.module.css';
 type SnackbarProps = {
     id: string,
 }
 export default function Snackbar(props: SnackbarProps) {
     return (
-        <div className='snackbar' id={props.id}></div>
+        <div className={styles.Snackbar} id={props.id}></div>
     )
 }
