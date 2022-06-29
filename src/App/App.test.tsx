9692177891a824195877f3de052e69aae9492525
Emilia Starczyk <emiliastarczyk@gmail.com>
2022-06-29 16:15:13 +0200

    app test for loading/data/error state

--- src/App/App.test.tsx
+++ src/App/App.test.tsx
@@ -1,9 +1,24 @@
 import App from './App';
 import '@testing-library/jest-dom/extend-expect';
-import { render, cleanup, screen } from '@testing-library/react';
-import React from 'react';
+import { render, screen, waitForElementToBeRemoved } from '@testing-library/react';
+const data = [
+  {
+    "userId": 1,
+    "id": 1,
+    "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+    "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+  },
+  {
+    "userId": 1,
+    "id": 2,
+    "title": "qui est esse",
+    "body": "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
+  }]
+const initOkRespone = { "status": 200, "statusText": "", "ok": true };
 
-afterAll(() => { jest.spyOn(React, 'useState').mockRestore(); cleanup(); })
+afterEach(() => {
+  jest.restoreAllMocks();
+});
 
 test('renders without errors', () => {
   render(<App />);
@@ -17,11 +32,23 @@ test('if loading exists', async () => {
 });
 
 test('if data exists', async () => {
-  const data = { title: 'some title' }
-
-  React.useState = jest.fn().mockReturnValue([data, {}])
+  global.fetch = jest.fn().mockResolvedValue(
+    new Response(JSON.stringify(data), initOkRespone)
+  );
   render(<App />);
-
+  await waitForElementToBeRemoved(() => screen.queryByTestId(/loadingTest/));
   const text = screen.getByTestId(/dataTest/);
   expect(text).toBeInTheDocument();
 });
+
+test('if error exists', async () => {
+  global.fetch = jest.fn().mockResolvedValue(
+    Promise.reject()
+  );
+  render(<App />);
+  await waitForElementToBeRemoved(() => screen.queryByTestId(/loadingTest/));
+  const text = screen.getByTestId(/errorTest/);
+  expect(text).toBeInTheDocument();
+  expect(text).toHaveTextContent('Ops. Something went wrong.');
+});
+
